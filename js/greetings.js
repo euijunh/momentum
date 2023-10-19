10b7d3c691bdeefd96ba0d7131c03eac461f9f04
euijunh <euijunheo@naver.com>
2023-10-20 01:33:01 +0900

    [i added] add logout function

--- js/greetings.js
+++ js/greetings.js
@@ -1,13 +1,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const logoutButton = document.querySelector("#logout-button");
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
+  loginInput.value = "";
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   localStorage.setItem(USERNAME_KEY, username);
   paintGreetings(username);
   showTodo();
@@ -16,6 +18,7 @@ function onLoginSubmit(event) {
 function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
+  logoutButton.classList.remove(HIDDEN_CLASSNAME);
 }
 
 function showTodo() {
@@ -23,12 +26,28 @@ function showTodo() {
   document.querySelector("#todo-list").classList.remove(HIDDEN_CLASSNAME);
 }
 
+function onLogout() {
+  todoInput.value = "";
+  todoList.innerHTML = "";
+  localStorage.removeItem(USERNAME_KEY);
+  localStorage.removeItem("todos");
+  todoForm.classList.add(HIDDEN_CLASSNAME);
+  todoList.classList.add(HIDDEN_CLASSNAME);
+  greeting.classList.add(HIDDEN_CLASSNAME);
+  logoutButton.classList.add(HIDDEN_CLASSNAME);
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+}
+
 const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+logoutButton.addEventListener("click", onLogout);
 
 if(savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
+  logoutButton.classList.remove(HIDDEN_CLASSNAME);
   paintGreetings(savedUsername);
   showTodo();
 }
